d90b9d703c3a665d49b53e3f1014fc29e7d02603
briz1 <my1queen8my1king@gmail.com>
2023-12-26 12:50:34 +0700

    First task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");//Выведите в консоль количество месяцев, необходимое для накопления заданной суммы
+        int deposit = 15000;
+        int total = 0;
+        int i = 0;
+        while (total<2_459_000) {
+            total=total+total/100;
+            total=total+deposit;
+            if (i >= 0 && i<97) {
+                i++;
+            continue;
+        }
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        System.out.println("   ");
+        System.out.println("Задача 2");//Напишите программу, которая выводит в консоль заданную последовательность чисел
+
+
+
     }
 }
